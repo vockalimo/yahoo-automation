fb22a3fe1941d8da77910cebacf93728e5b14fd1
vocka <vockalimo@gmail.com>
2022-01-22 18:49:11 +0800

    chagne report

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,8 +29,8 @@ const config: PlaywrightTestConfig = {
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-
+ // reporter: 'html',
+  reporter: process.env.CI ? 'github' : 'list', 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
 
